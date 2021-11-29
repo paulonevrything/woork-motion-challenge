50d45e220a04290bf1cd4d5370ff0f9335cb788b
Paul Olabisi <polabisi@paga.com>
2021-11-29 03:44:56 +0100

    added swagger config

---
+++ src/main/java/com/peopleflow/restapi/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.peopleflow.restapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig  implements WebMvcConfigurer {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(metaData())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.peopleflow.restapi.controllers"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    private ApiInfo metaData() {
+        ApiInfo apiInfo = new ApiInfo(
+                "WorkMotion Code Challenge REST API",
+                "API Project for WorkMotion Code Challenge",
+                "1.0",
+                "Terms of service",
+                "Paul Olabisi",
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0");
+        return apiInfo;
+    }
+}
